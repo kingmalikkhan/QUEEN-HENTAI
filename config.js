ac4afc87837b4a9b4084d9eeb28a0093623b4032
kingmalikkhan <malikkhanusman1@gmail.com>
2024-08-28 12:30:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['923436062975', 'malikkhan', true],
+  ['923436062975', 'malikkhan', true], 
   [''] 
 ] //Number of owners
 
